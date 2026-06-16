88d1c6ca09af94edc412a400f83b61b1bbe969b8
Five-HZ <huanzhou.wu@metax-tech.com>
2026-06-16 10:42:14 +0000

    install z3-solver into cache dir

--- scripts/ci/setup_nightly_venv.sh
+++ scripts/ci/setup_nightly_venv.sh
@@ -25,12 +25,12 @@ source "${VENV_PATH}/bin/activate"
 python -m pip install --upgrade pip setuptools wheel --no-user
 
 # Install tilelang-metax and tvm-ffi
-pip install -r 3rdparty/tilelang-metax/requirements.txt
 pip install build
 python -m build -w 3rdparty/tilelang-metax
-pip install --target=/data/cache/site-packages --no-deps --upgrade 3rdparty/tilelang-metax/dist/tilelang-0.1.9+maca.gitf7451dfc-cp38-abi3-linux_x86_64.whl
+pip install --target=/data/cache/site-packages --no-deps 3rdparty/tilelang-metax/dist/tilelang-0.1.9+maca.gitf7451dfc-cp38-abi3-linux_x86_64.whl
+pip install --target=/data/cache/site-packages --no-deps "z3-solver>=4.13.0,<4.15.5"
 python -m build -w 3rdparty/tilelang-metax/3rdparty/tvm/3rdparty/tvm-ffi
-pip install --target=/data/cache/site-packages --no-deps --upgrade 3rdparty/tilelang-metax/3rdparty/tvm/3rdparty/tvm-ffi/dist/apache_tvm_ffi-0.1.3.dev11+gae346ec92.d20260616-cp312-abi3-linux_x86_64.whl
+pip install --target=/data/cache/site-packages --no-deps 3rdparty/tilelang-metax/3rdparty/tvm/3rdparty/tvm-ffi/dist/apache_tvm_ffi-0.1.3.dev11+gae346ec92.d20260616-cp312-abi3-linux_x86_64.whl
 pip install --target=/data/cache/site-packages --python-version 3.10.0 --no-deps flash-linear-attention==0.4.0 -i https://repos.metax-tech.com/r/maca-pypi/simple --trusted-host repos.metax-tech.com
 
 {
