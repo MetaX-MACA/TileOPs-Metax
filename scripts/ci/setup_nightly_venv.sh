01211662ba693a3cf87c050012a244a1b3f30be3
Five-HZ <huanzhou.wu@metax-tech.com>
2026-06-17 09:21:27 +0000

    set local_scheme for build tvm-ffi

--- scripts/ci/setup_nightly_venv.sh
+++ scripts/ci/setup_nightly_venv.sh
@@ -29,8 +29,8 @@ pip install build
 python -m build -w 3rdparty/tilelang-metax
 pip install --target=/data/cache/site-packages --no-deps 3rdparty/tilelang-metax/dist/tilelang-0.1.9+maca.gitf7451dfc-cp38-abi3-linux_x86_64.whl
 pip install --target=/data/cache/site-packages --no-deps "z3-solver>=4.13.0,<4.15.5"
-python -m build -w 3rdparty/tilelang-metax/3rdparty/tvm/3rdparty/tvm-ffi
-pip install --target=/data/cache/site-packages --no-deps 3rdparty/tilelang-metax/3rdparty/tvm/3rdparty/tvm-ffi/dist/apache_tvm_ffi-0.1.3.dev11+gae346ec92.d20260616-cp312-abi3-linux_x86_64.whl
+SETUPTOOLS_SCM_OVERRIDES_FOR_APACHE_TVM_FFI='{local_scheme="no-local-version"}' python -m build -w 3rdparty/tilelang-metax/3rdparty/tvm/3rdparty/tvm-ffi
+pip install --target=/data/cache/site-packages --no-deps 3rdparty/tilelang-metax/3rdparty/tvm/3rdparty/tvm-ffi/dist/apache_tvm_ffi-0.1.3.dev11-cp312-abi3-linux_x86_64.whl
 pip install --target=/data/cache/site-packages --python-version 3.10.0 --no-deps flash-linear-attention==0.4.0 -i https://repos.metax-tech.com/r/maca-pypi/simple --trusted-host repos.metax-tech.com
 
 {
