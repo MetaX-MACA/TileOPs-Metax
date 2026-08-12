b39eb36f15ffd776cff4883810b5174d448918e2
guangxing83 <2021302121427@whu.edu.cn>
2026-08-12 09:24:49 +0000

    remove some case from xfail whitelist

--- tests/xfail_whitelist.py
+++ tests/xfail_whitelist.py
@@ -54,8 +54,6 @@ _MACA_XFAIL_GROUPS: tuple[tuple[str, tuple[str, ...]], ...] = (
             "tests/ops/attention/test_gqa_sliding_window_varlen.py::test_gqa_sliding_window_varlen_fwd_op[3-seqlens_q11-seqlens_k11-8-2-64-True--1--1-dtype11-False]",
             "tests/ops/attention/test_gqa_sliding_window_varlen.py::test_gqa_sliding_window_varlen_fwd_op[2-seqlens_q12-seqlens_k12-8-2-64-False--1-64-dtype12-False]",
             "tests/ops/attention/test_gqa_sliding_window_varlen.py::test_gqa_sliding_window_varlen_fwd_op[2-seqlens_q13-seqlens_k13-8-2-64-True-0--1-dtype13-False]",
-            "tests/ops/attention/test_mha.py::test_mha_bwd[smoke-bwd-fp16]",
-            "tests/ops/attention/test_mha.py::test_mha_bwd[smoke-bwd-bf16]",
             "tests/ops/attention/test_mha.py::test_mha_bwd[full-bwd-fp16-large]",
             "tests/ops/attention/test_mha.py::test_mha_bwd[full-bwd-bf16-tuned]",
             "tests/ops/test_kernel_selection.py::test_paged_prefill_fp8_cache_dispatch_is_unchanged",
